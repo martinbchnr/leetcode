141a10d23141825315a26dca4e05408d3415a92f
Martin Büchner <buechner@cs.uni-freiburg.de>
2026-04-02 12:24:19 +0200

    thinkpad

---
+++ top150/binary-tree/101.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isMirror(self, t1, t2) -> bool:
+        if not t1 and not t2:
+            return True
+        
+        if not t1 or not t2 or t1.val != t2.val:
+            return False
+
+        return self.isMirror(t1.left, t2.right) and self.isMirror(t1.right, t2.left)
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+
+        return self.isMirror(root.left, root.right)
